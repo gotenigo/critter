c06ab6d7d54bf5aaa6680c97fbc01db03beeb501
gotenigo <74220442+gotenigo@users.noreply.github.com>
2022-02-13 21:46:32 +0000

    completed

--- src/main/java/com/udacity/jdnd/course3/critter/util/util.java
+++ src/main/java/com/udacity/jdnd/course3/critter/util/util.java
@@ -1,15 +1,7 @@
 package com.udacity.jdnd.course3.critter.util;
 
-import com.udacity.jdnd.course3.critter.pet.domain.Pet;
-import com.udacity.jdnd.course3.critter.pet.domain.PetDTO;
-import com.udacity.jdnd.course3.critter.schedule.domain.Schedule;
-import com.udacity.jdnd.course3.critter.schedule.domain.ScheduleDTO;
-import com.udacity.jdnd.course3.critter.user.domain.*;
-import org.springframework.beans.BeanUtils;
 
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
+import org.springframework.beans.BeanUtils;
 
 
 public class util {
